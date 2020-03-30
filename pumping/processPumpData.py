d4569bad7d75a70e6fc48c4fa757e319c8586e28
iordach1 <iordach1@illinois.edu>
2020-03-30 15:05:18 -0500

    add model bounds
    they are maybe excessive

--- pumping/processPumpData.py
+++ pumping/processPumpData.py
@@ -5,3 +5,10 @@ Created on Mon Mar 16 14:48:57 2020
 @author: iordach1
 """
 
+import pandas as pd
+
+input_file = "Pumpage_Data_GRL.csv"
+
+df = pd.read_csv(input_file, dtype = {'isws_facility_id': str})
+
+pivot_df = 
