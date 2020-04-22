6725fe36688b8d6108f22a55c1862be282f07def
iordach1 <iordach1@illinois.edu>
2020-04-22 16:32:07 -0500

    add some new pumpage proc
    essing requirements

--- pumping/processPumpData.py
+++ pumping/processPumpData.py
@@ -31,6 +31,20 @@ for row in df.iterrows():
 df.reset_index(inplace = True, drop = True)
 print("Removed wells without pumpage data")
 
+for row in df.iterrows():
+    if pd.isnull(row[1][4]) or row[1][4] < 0:
+        df.drop(row[0], inplace = True)
+        
+df.reset_index(inplace = True, drop = True)
+print("Removed wells without depth")
+
+for row in df.iterrows():
+    if row[1][1][3] == '7':
+        df.drop(row[0], inplace = True)
+        
+df.reset_index(inplace = True, drop = True)
+print("Removed ag irrigation wells")
+
 trans_df = df.transpose()
 
 df2 = trans_df.iloc[7:].astype('float').interpolate(limit_area = 'inside')
@@ -49,15 +63,15 @@ df.to_csv("Pumpage_Data_GRL_mod.csv")
 #%%plots
 
 
-for row in df.sample(5).iterrows():
-    plt.figure(figsize=(17,11))
-    plt.plot([x for x in range(1981, 2019)], row[1][7:], '--', label = 'mod', linewidth =3)
-    plt.plot([x for x in range(1981, 2019)], static_df[static_df['p_num'] == row[1][0]].iloc[:,7:].values.tolist()[0], label = 'orig', linewidth = 3)
-    plt.title("Well p_num: {0}".format(row[1][0]))
-    plt.xlim(1981, 2018)
-    plt.legend()
-    plt.savefig("{0}.jpg".format(row[1][0]))
-    plt.clf()
+# for row in df.sample(5).iterrows():
+#     plt.figure(figsize=(17,11))
+#     plt.plot([x for x in range(1981, 2019)], row[1][7:], '--', label = 'mod', linewidth =3)
+#     plt.plot([x for x in range(1981, 2019)], static_df[static_df['p_num'] == row[1][0]].iloc[:,7:].values.tolist()[0], label = 'orig', linewidth = 3)
+#     plt.title("Well p_num: {0}".format(row[1][0]))
+#     plt.xlim(1981, 2018)
+#     plt.legend()
+#     plt.savefig("{0}.jpg".format(row[1][0]))
+#     plt.clf()
     
 
     
